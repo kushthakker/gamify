45fcd29c98f81f31a5975e4cd03decaa4c1e50d8
kushthakker <kushthakker777@gmail.com>
2021-08-05 19:12:53 +0530

    error boundary

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const App = () => {
-  return <div>BoilerPlate</div>;
+  function ErrorFallback({ error, resetErrorBoundary }) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <ErrorBoundary
+        fallBackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+      >
+        <div>BoilerPlate</div>
+      </ErrorBoundary>
+    </div>
+  );
 };
 
 export default App;
