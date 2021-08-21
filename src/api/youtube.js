c5e8e721abf2aea1c18c245c16bf13b8c2d57fa6
kushthakker <kushthakker777@gmail.com>
2021-08-21 15:17:41 +0530

    removed kocal baseURL

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,8 +2,7 @@ import axios from "axios";
 const KEY = "AIzaSyAsRjPfiG75tEnJytM3s36609CpxpTbWHg";
 // const KEY = process.env.YT_API;
 export default axios.create({
-  baseURL:
-    "https://secret-sierra-81136.herokuapp.com/https://www.googleapis.com/youtube/v3",
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 4,
