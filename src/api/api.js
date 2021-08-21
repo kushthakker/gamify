f0d65335695605634fa239376a540c6dc0fd5509
kushthakker <kushthakker777@gmail.com>
2021-08-21 15:26:48 +0530

    bug

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const KEY = process.env.GAME_API;
+const KEY = "9add24d57d554279baa4f2046c96bf99";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: process.env.GAME_API,
+    key: KEY,
   },
 });
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-// const KEY = process.env.YT_API;
+const KEY = "AIzaSyAsRjPfiG75tEnJytM3s36609CpxpTbWHg";
+
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 4,
-    key: process.env.YT_API,
+    key: KEY,
     type: "video",
   },
 });
