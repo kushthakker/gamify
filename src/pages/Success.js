b243f069a6e87908332d0addefd34dfa9e62bdec
kushthakker <kushthakker777@gmail.com>
2021-08-26 01:47:52 +0530

    chakazzz

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -6,20 +6,29 @@ import styled from "@emotion/styled/macro";
 
 const Heading = styled.h1({
   fontSize: "6rem",
-  fontFamily: "Pacifico",
+  fontFamily: "Staatliches",
 });
 
 const Success = () => {
   const [redirect, setRedirect] = React.useState(false);
+  const [timeLeft, setTimeLeft] = React.useState(5);
   React.useEffect(() => {
-    const timer = setTimeout(() => setRedirect(true), 3000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+    const timer = setTimeout(() => {
+      setRedirect(true);
+    }, 5000);
+    const countdown = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+    if (timeLeft === 0)
+      return () => {
+        clearTimeout(timer);
+        clearInterval(countdown);
+      };
+  }, [timeLeft]);
+
   return (
     <div>
-      {redirect ? (
+      {redirect && timeLeft === 1 ? (
         (window.location.href = "/")
       ) : (
         <div
@@ -33,7 +42,7 @@ const Success = () => {
             color: "black",
           }}
         >
-          <Heading>Redirecting...</Heading>
+          <Heading>Redirecting in {timeLeft}</Heading>
         </div>
       )}
     </div>
